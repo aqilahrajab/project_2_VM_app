e5d5cae1dd60227b548d8c26d944d91de8be71fa
aqilahrajab <YOUR-EMAIL-ADDRESS>
2019-08-04 20:56:25 +0800

    Login page complete (function + css)

--- views/login.jsx
+++ views/login.jsx
@@ -1,16 +1,35 @@
 var React = require("react");
+var bootstrapMeta = require('./bootstrap');
 
 class Login extends React.Component {
     render() {
+
+               var logoStyle = {
+                height:'150px',
+                width:'150px',
+            }
+
+
         return (
-            <html>
-                <h1> LOGIN </h1>
-                    <form method="POST" action="/login">
-                        <p><input type ="text" name="email" placeholder ="E-MAIL"/></p>
-                        <p><input type ="text" name="password" placeholder ="PASSWORD"/></p>
-                        <p><button type = "Submit">LOGIN</button></p>
-                    </form>
-            </html>
+            <bootstrapMeta>
+                <html>
+                <head>
+                <link rel="stylesheet" type="text/css" href="css/login.css"/>
+                </head>
+
+                    <div className="box">
+                        <div className = "form-content">
+                           <img src="images/mlogo-1.png" style={logoStyle} alt="Company Logo"/>
+                            <form method="POST" action="/login">
+                                <p><input type ="text" name="email" placeholder ="E-MAIL"/></p>
+                                <p><input type ="text" name="password" placeholder ="PASSWORD" className='password'/></p>
+                                <p><button type = "Submit">LOGIN</button></p>
+                            </form>
+                        </div>
+                    </div>
+
+                </html>
+            </bootstrapMeta>
         )
     }
 }
